b4c5511fc4a9ff7092b660ab756c7c8d5110ac56
Taylor Monacelli <taylormonacelli@gmail.com>
2022-11-13 08:13:28 -0800

    real men dont make git commit comments

--- script1.ps1
+++ script1.ps1
@@ -19,12 +19,21 @@ function Get-FileIfNotExists {
 $url = "https://software-download.microsoft.com/download/pr/19042.508.200927-1902.20h2_release_svc_refresh_CLIENTENTERPRISEEVAL_OEMRET_x64FRE_en-us.iso"
 $filename = Split-Path -Leaf -Path $url
 $fileinfo = New-Object System.IO.FileInfo($filename)
-$basedir = '~/Downloads'
 $iso = Join-Path "${HOME}/Downloads" $filename
 Get-FileIfNotExists $url $iso
 $global:ProgressPreference = "SilentlyContinue"
 Mount-DiskImage -ImagePath $iso
 
 New-Item -Type "directory" -Force -Path sxs | Out-Null
-Copy-Item -Force -Recurse e:/sources/sxs/* ./sxs
-Get-ChildItem -Recurse ./sxs | Select-Object -Expand Fullname
+
+for ($test = 0; $test -lt 26; $test++)
+{
+    $drive=[char](65 + $test)
+    $sxs = "${drive}:/sources/sxs"
+    If(Test-Path($sxs)){
+        Copy-Item -Force -Recurse "${sxs}/*"./sxs
+        Get-ChildItem -Recurse ./sxs | Select-Object -Expand Fullname
+        break
+    }
+}
+
