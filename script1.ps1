ed2bb59c5d3c0d20034a5e069dba311d96610507
Taylor Monacelli <taylormonacelli@gmail.com>
2022-11-13 07:32:20 -0800

    real men dont make git commit comments

--- script1.ps1
+++ script1.ps1
@@ -1,8 +1,29 @@
+function Get-FileIfNotExists {
+    Param (
+        $Url,
+        $Destination
+    )
+
+    if (-not (Test-Path $Destination)) {
+        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+        $ProgressPreference = 'SilentlyContinue'
+
+        Write-Verbose "Downloading $Url"
+        Invoke-WebRequest -UseBasicParsing -Uri $url -Outfile $Destination
+    }
+    else {
+        Write-Verbose "${Destination} already exists. Skipping."
+    }
+}
+
 $url = "https://software-download.microsoft.com/download/pr/19042.508.200927-1902.20h2_release_svc_refresh_CLIENTENTERPRISEEVAL_OEMRET_x64FRE_en-us.iso"
 $filename = Split-Path -Leaf -Path $url
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$ProgressPreference = 'SilentlyContinue'
-Invoke-WebRequest -UseBasicParsing -Uri $url -Outfile $filename
+$fileinfo = New-Object System.IO.FileInfo($filename)
+Get-FileIfNotExists $url $filename
+$global:ProgressPreference = "SilentlyContinue"
 $iso = Join-Path (pwd) $filename
 Mount-DiskImage -ImagePath $iso
-Get-ChildItem -Recurse d:\sources\sxs | Select-Object -Expand Fullname
+
+New-Item -Type "directory" -Force -Path sxs | Out-Null
+Copy-Item -Force -Recurse d:/sources/sxs/* ./sxs
+Get-ChildItem -Recurse ./sxs | Select-Object -Expand Fullname
