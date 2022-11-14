7c1c1d41d611c65e45647b2cf94811fafc41f761
Taylor Monacelli <taylormonacelli@gmail.com>
2022-11-13 22:29:47 -0800

    real men dont make git commit comments

--- wixtoolset.ps1
+++ wixtoolset.ps1
@@ -1,4 +1,4 @@
 choco install wixtoolset
 $wix_dir = (Get-ChildItem -Recurse C:\Program*\Wix*Toolset*\bin -Filter "heat.exe" | select-object -first 1).Directory.FullName
 $env:PATH = "$wix_dir;$env:PATH"
-heat
+heat1
